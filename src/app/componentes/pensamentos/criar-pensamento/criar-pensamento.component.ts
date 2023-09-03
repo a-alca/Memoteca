113cd312f39649a9ee4f6655eb6bbc69511b328a
André Alcântara <106185003+a-alca@users.noreply.github.com>
2023-09-03 17:09:29 -0300

    property binding - interpolação
    Utilização de porperty binding para renderização de atributos e propriedades no HTML
    [value] da tag <input>
    {{ atributo.propriedade }} na tag <p> chamado de INTERPOLACAO

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.css']
 })
-export class CriarPensamentoComponent {
+export class CriarPensamentoComponent implements OnInit {
+
+pensamento = { // atributo pensamento, esse atributo é um objeto que possui as propriedades: id, conteudo, autoria e modelo. // como associar esse atributo ao input de pensamento, autoria e modelo lá nas tags input do HTML >> ver linha 13 [value] do criar-pensamento.component.html.
+  id: '1',
+  conteudo: 'Aprendendo Angular',
+  autoria: 'Dev',
+  modelo: ''
+}
+
+constructor() {  }
+
+ngOnInit(): void {
+
+}
 
 }
