197828264cc2ebcaaf5d5f937d02a6f43a7cd839
André Alcântara <106185003+a-alca@users.noreply.github.com>
2023-09-03 17:38:40 -0300

    event binding
    EVENT BINDINHG - ao contrario do property binding que renderiza no HTML propriedades e atributos do component.ts, o EVENT BINDING enventListenner que me permite criar metodos e funcoes advindas de interacao com o frontend, no exemplo desse exercicio eu criei o event binding no botao salvar com (click) e ao clicar no botao Salvar, criara a funcao criarPensamento() que chamado da mesma funcao declarada em criar-pensamento.component.ts que retorna o comportamento de um alert com mensagem de Novo pensamento criado.

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+pensamento = { // atributo pensamento, esse atributo é um objeto que possui as propriedades: id, conteudo, autoria e modelo. // como associar esse atributo ao input de pensamento, autoria e modelo lá nas tags input do HTML >> ver linha 13 [value] do criar-pensamento.component.html.
+  id: '1',
+  conteudo: 'Aprendendo Angular',
+  autoria: 'Dev',
+  modelo: ''
+}
+
+constructor() {  }
+
+ngOnInit(): void {
+
+}
+
+criarPensamento() {
+  alert("Novo pensamento criado")
+}
+
+}
