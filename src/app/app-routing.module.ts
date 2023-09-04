c12fad708bbc1c6b0107b8c0058c96600da2f0a7
André Alcântara <106185003+a-alca@users.noreply.github.com>
2023-09-03 22:52:41 -0300

    Configuracao de rotas app-rputing.module.ts
    criação de caminhos listarPensamento, criarPensamento e redirectTo para path vazio com pactMatch full ;) Proxima para RouterLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+// Constante routes representando as rotas da aplicacao, do tipo Route que é um objeto de configuracao que define as rotas. Na nossa aplicacao havera um arrey de rotas
+const routes: Routes = [
+  {
+    path: '', // sempre que criar uma propriedade com caminho vazio, é necessário crirar outra propriedade, o pathMatch, como abaixo:
+    redirectTo: 'listarPensamento',
+    pathMatch: 'full' // essa propriedade possui dois valores, o prefix e o full, pq a URL é lida da esquerda para direita, etao se colocar o valor de prefix é considerado apenas o localhost:4200 e desconsidera o resto, porém caso escolha full, é lida toda url e feito o redirecinamento para os componentes.
+  },
+  {
+    path: 'criarPensamento', // esse path é o caminho para rota que eu quero acessar - quando a rota criarPensamento for acessada, o que eu quero que carregue? o componente abaixo:
+    component: CriarPensamentoComponent
+  },
+  {
+    path: 'listarPensamento',
+    component: ListarPensamentoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
