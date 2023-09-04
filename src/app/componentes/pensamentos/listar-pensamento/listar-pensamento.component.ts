1461d16497fcff0a3aa20d1a28d86b295759498f
André Alcântara <106185003+a-alca@users.noreply.github.com>
2023-09-04 16:43:42 -0300

    Add files via upload
    Para entendimento da diretiva ngClass, fiz a utilização dela para incluir a classe de tamanho de um elemento no template e a solução para renderizacao de layout distinto entre pensamento-p (pequeno e -g para grande) vou retirar a class pensamento-p e incluir a diretiva ngClass e para isso, apos criar o metodo larguraPensamento() em pensamento.component.ts vou incluir aqui o [ngClass]="larguraPensamento()
    
    com a solução abaixo no pensamento.componet.ts
    
      larguraPensamento(): string {
        if(this.pensamento.conteudo.length >= 256) {
          return 'pensamento-g'
        }
        return 'pensamento-p'
      }

---
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { PensamentoComponent } from '../pensamento/pensamento.component';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  templateUrl: './listar-pensamento.component.html',
+  styleUrls: ['./listar-pensamento.component.css']
+})
+export class ListarPensamentoComponent implements OnInit {
+
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho.',
+      autoria: 'Componente pai',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input().',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: `Pensamento gerado na aula ngClass para renderizar esses cards conforme metodo larguraPesamento() no arquivo pensamento.component.ts
+      larguraPensamento(): string {
+        if(this.pensamento.conteudo.length >= 256) {
+          return 'pensamento-g'
+        }
+        return 'pensamento-p'
+      }`,
+      autoria: 'Componente pai',
+      modelo: 'modelo3'
+    }
+  ];
+
+  constructor() {  }
+
+  ngOnInit(): void {
+
+  }
+}
---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  @Input() pensamento = { // atributo chamado pensamento, ele é um objeto //A diretiva @Input está dizendo que esse atribuo agora irá receber informacoes do componente pai.
+    conteudo: 'Winter is coming',
+    autoria: 'Ned',
+    modelo: 'modelo2'
+  }
+
+  constructor() {  }
+
+  ngOnInit(): void {
+
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+
+}
