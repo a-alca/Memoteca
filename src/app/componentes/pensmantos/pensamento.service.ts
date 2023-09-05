73b9c4cb37ef6bb3c8a4cbfc05feafe67db35815
André Alcântara <106185003+a-alca@users.noreply.github.com>
2023-09-04 21:09:09 -0300

    Injeção de dependência
    Para entender a Injeção de dependência, no arquivo pensamento.service.ts criado anteriormente, no construtor
    
    constructor(private http: HttpClient) { }

--- src/app/componentes/pensmantos/pensamento.service.ts
+++ src/app/componentes/pensmantos/pensamento.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ // esse decoretor informa para o angular que essa é uma classe injetavel, ou seja, ela pode ser utilizada em outras depedencias.. em outras classes.. em outros compoentes, atraves de um metodo chamado de injecao de dependencias assunto do proximo commit.
   providedIn: 'root'
 })
 export class PensamentoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { } // com private o atributo é privado do PensamentoService
+
 }
