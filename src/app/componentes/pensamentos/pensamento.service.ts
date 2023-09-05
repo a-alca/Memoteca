df44517da573506fcc75a3e27503a62237c88b06
André Alcântara <106185003+a-alca@users.noreply.github.com>
2023-09-05 17:59:32 -0300

    CRUD update
    Nessa atividade tive a oportunidade de aplicar mais um das 4 operações básicas no Angular, o update.
    
    1 Após a criação do componente de editarPensamento, criei a função editar() em pensamento.service.ts
    2 Estilizei o css e HTML
    3 Em editar-pensamento.component.ts criei a propriedade pensamento com estrutura de pensamento
      >> No constructor informei as propriedades de service e route
      >> No ngOnInit criei a constante id para mapear o pensamento, dar um get no id e invoquei a buscarPorId que será subscrito no pensamento mapeado
      >> Criei a função para o botão do HTML, editarPensamento() e o botão cancelar() com suas respectivas rotas this.router.navigate(['/listarPensamento'])
    4 Em app-routing.module.ts criei o path: 'pensamentos/editarPensamento/:id'
    5 No pensamento.component.html inclui no botão/imagem alterar o routerLink="/pensamentos/editarPensamento/{{pensamento.id}}"

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -18,6 +18,10 @@ export class PensamentoService {
   criar(pensamento: Pensamento): Observable<Pensamento> { // aqui para criar o pensamento, criar é um observable de pensamento (nao da lista pois vai criar um pensamento) como vai criar, (Created) é utilizado o metodo post do tipo Pensamento passando a API como e pensamento.
     return this.http.post<Pensamento>(this.API, pensamento)
   }
+  editar(pensamento: Pensamento): Observable<Pensamento> { // criei a funcao editar
+    const url = `${this.API}/${pensamento.id}` // criei a constante ulr para editar o pensamento da api
+    return this.http.put<Pensamento>(url, pensamento ) // e put a alteracao, depois vou para editar css e html...
+  }
   excluir(id: number): Observable<Pensamento> { // fiz o metodo PensamentoService.excluir e ele é um observable de Pensamento
     const url = `${this.API}/${id}` // criei a contstante chamada url pq alem de passsar a url http://localhost:3000/pensamentos, eu preciso acrecentar o id
     return this.http.delete<Pensamento>(url) // entao retrona o metodo delete do Pensamento respectivo a id encontrada sobre o pensamento deletado
